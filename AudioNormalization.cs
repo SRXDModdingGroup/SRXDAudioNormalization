4b864102058c417dc3fd41eed9ee039b60de60d8
Dominic Aglialoro <domiaglialoro@gmail.com>
2022-03-14 19:30:16 -0700

    Add files

---
+++ AudioNormalization.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using UnityEngine;
+using Utility;
+
+namespace SRXDAudioNormalization; 
+
+public static class AudioNormalization {
+    private static Dictionary<string, float> peaksForTrackData = new();
+
+    public static float GetVolumeMultiplierForTrackData(PlayableTrackData trackData) {
+        string hash = trackData.TrackInfoRef.UniqueName;
+
+        lock (peaksForTrackData) {
+            if (peaksForTrackData.TryGetValue(hash, out float peak)) {
+                if (peak < 0f)
+                    return 1f;
+                
+                return Mathf.Min(AudioUtils.DecibelToLinear(Plugin.PreferredLoudness.Value) / peak, AudioUtils.DecibelToLinear(Plugin.MaxGain.Value));
+            }
+        }
+        
+        if (trackData.RawAudioDataHash == 0 && !trackData.LoadAllRawClipInfo())
+            return 1f;
+        
+        AudioData clip = default;
+        bool found = false;
+        
+        foreach (var subData in trackData.TrackDataList) {
+            foreach (var assetReference in subData.clipInfoAssetReferences) {
+                if (!assetReference.TryGetLoadedAsset(out var result)
+                    || !result.clipAssetReference.TryGetHandle<AudioClip, AudioClipAssetHandle>(out var handle))
+                    continue;
+                
+                clip = handle.RawData;
+                found = true;
+
+                break;
+            }
+
+            if (found)
+                break;
+        }
+
+        if (!found)
+            return 1f;
+
+        float[] data = clip.Data;
+
+        if (data == null)
+            return 1f;
+        
+        lock (peaksForTrackData)
+            peaksForTrackData.Add(hash, -1f);
+
+        Task.Run(() => CalculatePeak(hash, clip.Data, clip.Samples, clip.Channels, clip.Frequency));
+
+        return 1f;
+    }
+
+    private static void CalculatePeak(string hash, float[] data, int samples, int channels, int frequency) {
+        float[] dataSquared = new float[samples];
+        double runningSum = 0d;
+        double peak = 0d;
+        int peakWidth = frequency / 4;
+
+        for (int i = 0, j = 0; i < data.Length; i += channels, j++) {
+            float max = 0f;
+            
+            for (int k = i; k < i + channels; k++) {
+                float sample = data[k];
+                float square = sample * sample;
+
+                if (square > max)
+                    max = square;
+            }
+
+            dataSquared[j] = max;
+            runningSum += max;
+
+            if (j >= peakWidth)
+                runningSum -= dataSquared[j - peakWidth];
+
+            if (runningSum > peak)
+                peak = runningSum;
+        }
+
+        peak = Math.Sqrt(peak / peakWidth);
+        
+        // Plugin.Logger.LogMessage($"RMS: {rms:0.000}");
+        // Plugin.Logger.LogMessage($"Peak: {peak:0.000}");
+
+        lock (peaksForTrackData)
+            peaksForTrackData[hash] = (float) peak;
+    }
+}
---
+++ Plugin.cs
@@ -0,0 +1,39 @@
+﻿using BepInEx;
+using BepInEx.Logging;
+using HarmonyLib;
+using SMU.Utilities;
+using SpinCore;
+using SpinCore.UI;
+using Utility;
+
+namespace SRXDAudioNormalization;
+
+[BepInDependency("com.pink.spinrhythm.moddingutils", "1.0.2")]
+[BepInDependency("com.pink.spinrhythm.spincore")]
+[BepInPlugin("SRXD.AudioNormalization", "AudioNormalization", "1.0.0.0")]
+public class Plugin : SpinPlugin {
+    public new static ManualLogSource Logger { get; private set; }
+    
+    public static Bindable<float> PreferredLoudness { get; private set; }
+    
+    public static Bindable<float> MaxGain { get; private set; }
+
+    protected override void Awake() {
+        base.Awake();
+
+        Logger = base.Logger;
+        PreferredLoudness = AddBindableConfig("PreferredLoudness", -4.0f);
+        MaxGain = AddBindableConfig("MaxGain", 3.5f);
+
+        var harmony = new Harmony("AudioNormalization");
+        
+        harmony.PatchAll(typeof(Patches));
+    }
+    
+    protected override void CreateMenus() {
+        var root = CreateOptionsTab("Audio Normalization").UIRoot;
+
+        SpinUI.CreateSlider("Preferred Loudness", root, -10f, 0f, valueDisplay: value => $"{value:F}db").Bind(PreferredLoudness);
+        SpinUI.CreateSlider("Maximum Gain", root, 0f, 5f, valueDisplay: value => $"{value:F}db").Bind(MaxGain);
+    }
+}
